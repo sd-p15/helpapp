0a660eba2aeb3ba68e72f30db5e2c47254383771
oliwia <oliwia.majewska01@gmail.com>
2020-11-30 10:21:20 +0100

    aktualizacja

--- WersjaGithubSzkody/app/src/main/java/com/example/KOPIA/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.example.KOPIA;
-
-public class Utils {
-    //pasy maila
-    public static final String EMAIL ="testapp7040@gmail.com";
-    public static final String PASSWORD ="t4y5u6i7";
-}
