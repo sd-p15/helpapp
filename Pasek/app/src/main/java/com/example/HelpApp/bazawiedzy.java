b417c79b96221918c3ae56d07169ac56b8f6a299
Natalia <12904@student.sgsp.edu.pl>
2021-01-18 11:33:31 +0100

    Dodany górny pasek

---
+++ Pasek/app/src/main/java/com/example/HelpApp/bazawiedzy.java
@@ -0,0 +1,391 @@
+package com.example.HelpApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class bazawiedzy extends AppCompatActivity {
+
+    Integer numerCiekawostki;
+    public static final String CIEKAWOSTKA_NO = "Bazawiedzy.no";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bazawiedzy);
+
+        Button c00 = findViewById(R.id.ciekawostka00);
+        Button c01 = findViewById(R.id.ciekawostka01);
+        Button c02 = findViewById(R.id.ciekawostka02);
+        Button c03 = findViewById(R.id.ciekawostka03);
+        Button c04 = findViewById(R.id.ciekawostka04);
+        Button c05 = findViewById(R.id.ciekawostka05);
+        Button c06 = findViewById(R.id.ciekawostka06);
+        Button c07 = findViewById(R.id.ciekawostka07);
+        Button c08 = findViewById(R.id.ciekawostka08);
+        Button c09 = findViewById(R.id.ciekawostka09);
+        Button c10 = findViewById(R.id.ciekawostka10);
+        Button c11 = findViewById(R.id.ciekawostka11);
+        Button c12 = findViewById(R.id.ciekawostka12);
+        Button c13 = findViewById(R.id.ciekawostka13);
+        Button c14 = findViewById(R.id.ciekawostka14);
+        Button c15 = findViewById(R.id.ciekawostka15);
+        Button c16 = findViewById(R.id.ciekawostka16);
+        Button c17 = findViewById(R.id.ciekawostka17);
+        Button c18 = findViewById(R.id.ciekawostka18);
+        Button c19 = findViewById(R.id.ciekawostka19);
+        Button c20 = findViewById(R.id.ciekawostka20);
+        Button c21 = findViewById(R.id.ciekawostka21);
+        Button c22 = findViewById(R.id.ciekawostka22);
+        Button c23 = findViewById(R.id.ciekawostka23);
+        Button c24 = findViewById(R.id.ciekawostka24);
+        Button c25 = findViewById(R.id.ciekawostka25);
+        Button c26 = findViewById(R.id.ciekawostka26);
+        Button c27 = findViewById(R.id.ciekawostka27);
+        Button c28 = findViewById(R.id.ciekawostka28);
+        Button c29 = findViewById(R.id.ciekawostka29);
+        Button c30 = findViewById(R.id.ciekawostka30);
+        Button c31 = findViewById(R.id.ciekawostka31);
+        Button c32 = findViewById(R.id.ciekawostka32);
+        Button c33 = findViewById(R.id.ciekawostka33);
+        Button c34 = findViewById(R.id.ciekawostka34);
+        Button c35 = findViewById(R.id.ciekawostka35);
+        Button c36 = findViewById(R.id.ciekawostka36);
+        Button c37 = findViewById(R.id.ciekawostka37);
+        Button c38 = findViewById(R.id.ciekawostka38);
+        Button c39 = findViewById(R.id.ciekawostka39);
+        Button c40 = findViewById(R.id.ciekawostka40);
+        Button c41 = findViewById(R.id.ciekawostka41);
+        Button c42 = findViewById(R.id.ciekawostka42);
+        Button c43 = findViewById(R.id.ciekawostka43);
+        Button c44 = findViewById(R.id.ciekawostka44);
+        Button c45 = findViewById(R.id.ciekawostka45);
+        Button c46 = findViewById(R.id.ciekawostka46);
+        Button c47 = findViewById(R.id.ciekawostka47);
+        Button c48 = findViewById(R.id.ciekawostka48);
+        Button c49 = findViewById(R.id.ciekawostka49);
+        Button c50 = findViewById(R.id.ciekawostka50);
+        Button c51 = findViewById(R.id.ciekawostka51);
+        Button c52 = findViewById(R.id.ciekawostka52);
+        Button c53 = findViewById(R.id.ciekawostka53);
+        Button c54 = findViewById(R.id.ciekawostka54);
+        Button c55 = findViewById(R.id.ciekawostka55);
+        Button c56 = findViewById(R.id.ciekawostka56);
+        Button c57 = findViewById(R.id.ciekawostka57);
+        Button c58 = findViewById(R.id.ciekawostka58);
+        Button c59 = findViewById(R.id.ciekawostka59);
+        
+
+        c00.setOnClickListener(v -> {
+            numerCiekawostki = 0;
+            openCiekawostka();
+        });
+
+        c01.setOnClickListener(v -> {
+            numerCiekawostki = 1;
+            openCiekawostka();
+        });
+
+        c02.setOnClickListener(v -> {
+            numerCiekawostki = 2;
+            openCiekawostka();
+        });
+
+        c03.setOnClickListener(v -> {
+            numerCiekawostki = 3;
+            openCiekawostka();
+        });
+
+        c04.setOnClickListener(v -> {
+            numerCiekawostki = 4;
+            openCiekawostka();
+        });
+
+        c05.setOnClickListener(v -> {
+            numerCiekawostki = 5;
+            openCiekawostka();
+        });
+
+        c06.setOnClickListener(v -> {
+            numerCiekawostki = 6;
+            openCiekawostka();
+        });
+
+        c07.setOnClickListener(v -> {
+            numerCiekawostki = 7;
+            openCiekawostka();
+        });
+
+        c08.setOnClickListener(v -> {
+            numerCiekawostki = 8;
+            openCiekawostka();
+        });
+
+        c09.setOnClickListener(v -> {
+            numerCiekawostki = 9;
+            openCiekawostka();
+        });
+
+        c10.setOnClickListener(v -> {
+            numerCiekawostki = 10;
+            openCiekawostka();
+        });
+
+        c11.setOnClickListener(v -> {
+            numerCiekawostki = 11;
+            openCiekawostka();
+        });
+
+        c12.setOnClickListener(v -> {
+            numerCiekawostki = 12;
+            openCiekawostka();
+        });
+
+        c13.setOnClickListener(v -> {
+            numerCiekawostki = 13;
+            openCiekawostka();
+        });
+
+        c14.setOnClickListener(v -> {
+            numerCiekawostki = 14;
+            openCiekawostka();
+        });
+
+        c15.setOnClickListener(v -> {
+            numerCiekawostki = 15;
+            openCiekawostka();
+        });
+
+        c16.setOnClickListener(v -> {
+            numerCiekawostki = 16;
+            openCiekawostka();
+        });
+
+        c17.setOnClickListener(v -> {
+            numerCiekawostki = 17;
+            openCiekawostka();
+        });
+
+        c18.setOnClickListener(v -> {
+            numerCiekawostki = 18;
+            openCiekawostka();
+        });
+
+        c19.setOnClickListener(v -> {
+            numerCiekawostki = 19;
+            openCiekawostka();
+        });
+
+        c20.setOnClickListener(v -> {
+            numerCiekawostki = 20;
+            openCiekawostka();
+        });
+
+        c21.setOnClickListener(v -> {
+            numerCiekawostki = 21;
+            openCiekawostka();
+        });
+
+        c22.setOnClickListener(v -> {
+            numerCiekawostki = 22;
+            openCiekawostka();
+        });
+
+        c23.setOnClickListener(v -> {
+            numerCiekawostki = 23;
+            openCiekawostka();
+        });
+
+        c24.setOnClickListener(v -> {
+            numerCiekawostki = 24;
+            openCiekawostka();
+        });
+
+        c25.setOnClickListener(v -> {
+            numerCiekawostki = 25;
+            openCiekawostka();
+        });
+
+        c26.setOnClickListener(v -> {
+            numerCiekawostki = 26;
+            openCiekawostka();
+        });
+
+        c27.setOnClickListener(v -> {
+            numerCiekawostki = 27;
+            openCiekawostka();
+        });
+
+        c28.setOnClickListener(v -> {
+            numerCiekawostki = 28;
+            openCiekawostka();
+        });
+
+        c29.setOnClickListener(v -> {
+            numerCiekawostki = 29;
+            openCiekawostka();
+        });
+
+        c30.setOnClickListener(v -> {
+            numerCiekawostki = 30;
+            openCiekawostka();
+        });
+
+        c31.setOnClickListener(v -> {
+            numerCiekawostki = 31;
+            openCiekawostka();
+        });
+
+        c32.setOnClickListener(v -> {
+            numerCiekawostki = 32;
+            openCiekawostka();
+        });
+
+        c33.setOnClickListener(v -> {
+            numerCiekawostki = 33;
+            openCiekawostka();
+        });
+
+        c34.setOnClickListener(v -> {
+            numerCiekawostki = 34;
+            openCiekawostka();
+        });
+
+        c35.setOnClickListener(v -> {
+            numerCiekawostki = 35;
+            openCiekawostka();
+        });
+
+        c36.setOnClickListener(v -> {
+            numerCiekawostki = 36;
+            openCiekawostka();
+        });
+
+        c37.setOnClickListener(v -> {
+            numerCiekawostki = 37;
+            openCiekawostka();
+        });
+
+        c38.setOnClickListener(v -> {
+            numerCiekawostki = 38;
+            openCiekawostka();
+        });
+
+        c39.setOnClickListener(v -> {
+            numerCiekawostki = 39;
+            openCiekawostka();
+        });
+
+        c40.setOnClickListener(v -> {
+            numerCiekawostki = 40;
+            openCiekawostka();
+        });
+
+        c41.setOnClickListener(v -> {
+            numerCiekawostki = 41;
+            openCiekawostka();
+        });
+
+        c42.setOnClickListener(v -> {
+            numerCiekawostki = 42;
+            openCiekawostka();
+        });
+
+        c43.setOnClickListener(v -> {
+            numerCiekawostki = 43;
+            openCiekawostka();
+        });
+
+        c44.setOnClickListener(v -> {
+            numerCiekawostki = 44;
+            openCiekawostka();
+        });
+
+        c45.setOnClickListener(v -> {
+            numerCiekawostki = 45;
+            openCiekawostka();
+        });
+
+        c46.setOnClickListener(v -> {
+            numerCiekawostki = 46;
+            openCiekawostka();
+        });
+
+        c47.setOnClickListener(v -> {
+            numerCiekawostki = 47;
+            openCiekawostka();
+        });
+
+        c48.setOnClickListener(v -> {
+            numerCiekawostki = 48;
+            openCiekawostka();
+        });
+
+        c49.setOnClickListener(v -> {
+            numerCiekawostki = 49;
+            openCiekawostka();
+        });
+
+        c50.setOnClickListener(v -> {
+            numerCiekawostki = 50;
+            openCiekawostka();
+        });
+
+        c51.setOnClickListener(v -> {
+            numerCiekawostki = 51;
+            openCiekawostka();
+        });
+
+        c52.setOnClickListener(v -> {
+            numerCiekawostki = 52;
+            openCiekawostka();
+        });
+
+        c53.setOnClickListener(v -> {
+            numerCiekawostki = 53;
+            openCiekawostka();
+        });
+
+        c54.setOnClickListener(v -> {
+            numerCiekawostki = 54;
+            openCiekawostka();
+        });
+
+        c55.setOnClickListener(v -> {
+            numerCiekawostki = 55;
+            openCiekawostka();
+        });
+
+        c56.setOnClickListener(v -> {
+            numerCiekawostki = 56;
+            openCiekawostka();
+        });
+
+        c57.setOnClickListener(v -> {
+            numerCiekawostki = 57;
+            openCiekawostka();
+        });
+
+        c58.setOnClickListener(v -> {
+            numerCiekawostki = 58;
+            openCiekawostka();
+        });
+
+        c59.setOnClickListener(v -> {
+            numerCiekawostki = 59;
+            openCiekawostka();
+        });
+
+
+        
+        
+    }
+
+    public void openCiekawostka() {
+        Intent intent = new Intent(this, ciekawostka.class);
+        intent.putExtra(CIEKAWOSTKA_NO, numerCiekawostki);
+        startActivity(intent);
+    }
+}
